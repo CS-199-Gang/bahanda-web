0f8a62b2df6f23fbc42c782634275e0d33fd281d
Kim Palao <kimfarhantpalao@gmail.com>
2022-04-11 00:31:11 +0800

    Add Laravel IDE Helper package

--- app/Models/UserType.php
+++ app/Models/UserType.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\UserType
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType whereName($value)
+ * @mixin \Eloquent
+ */
 class UserType extends Model
 {
     use HasFactory;
