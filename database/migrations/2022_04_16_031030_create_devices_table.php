7d5e33573afe14737b7c249bb896420215a4fb2a
Kim Palao <kimfarhantpalao@gmail.com>
2022-04-16 13:35:07 +0800

    Add Device model

---
+++ database/migrations/2022_04_16_031030_create_devices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->string('id', 64)->unique();
+            $table->primary('id');
+            $table->unsignedBigInteger('school_id');
+            $table->timestamps();
+
+            $table->foreign('school_id')->references('id')->on('schools');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
